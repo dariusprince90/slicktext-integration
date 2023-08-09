0b5b7350a79a8d43d360b7c158ffff61dd591d29
dariusprince90 <dariusprince90@gmail.com>
2023-08-09 11:03:08 -0400

    update welcome screen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,78 +1,38 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>API WORK</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-        <style>
-            table {
-                border-collapse: collapse;
-                width: 100%;
-            }
-            th, td {
-                border: 1px solid #ddd;
-                padding: 8px;
-                text-align: left;
-            }
-            th {
-                background-color: #f2f2f2;
-            }
-        </style>
-        <!-- Styles -->
-       
-    </head>
-    <body class="antialiased">
-        <h1>API DATA </h1>
-        <table>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Number</th>
-                    <th>City</th>
-                    <th>State</th>
-                    <th>Zipcode</th>
-                    <th>Country</th>
-                    <th>Textword</th>
-                    <th>Subscribed Date</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Birth Date</th>
-                    <th>Email</th>
-                    <th>Opt-In Method</th>
-                </tr>
-            </thead>
-            <tbody>
-            
-                <tr>
-                    <td>-</td>
-                    <td>-</td>
-                    <td>-</td>
-                    <td>-</td>
-                    <td>-</td>
-                    <td>-</td>
-                    <td>-</td>
-                    <td>2023-08-09</td>
-                    <td></td>
-                    <td></td>
-                    <td>1990-01-15</td>
-                    <td></td>
-                    <td>Email</td>
-                </tr>
-             
-            </tbody>
-        </table>
-           
+    <title>API WORK</title>
 
-              
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
 
+        th,
+        td {
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: left;
+        }
 
-              
-            
-        
-    </body>
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+    <!-- Styles -->
+
+</head>
+
+<body class="antialiased">
+    <h1>SlickText Contacts</h1>
+</body>
+
 </html>
